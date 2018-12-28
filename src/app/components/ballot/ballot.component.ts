f4040f3cd7d236b997cda3d4f8437be12fb2f3d1
Ian Barczewski <mercy-virtual+ib@asynchrony.com>
2018-12-27 23:12:20 -0600

    made ui changes - user must now enter a name and at least one game before submission, and the submit button disables when submitting the ballot until a response returns. if an error occurs, it displays on the page. - IB

--- src/app/components/ballot/ballot.component.ts
+++ src/app/components/ballot/ballot.component.ts
@@ -3,6 +3,8 @@ import {eligibleGames, eligibleRemasters } from '../../assets/gamelist';
 import { SubmissionService } from 'src/app/services/submission/submission.service';
 import { Ballot } from 'src/app/models/ballot';
 import { Choice } from 'src/app/models/choice';
+import { SubmissionState } from 'src/app/models/submissionState';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-ballot',
@@ -10,8 +12,10 @@ import { Choice } from 'src/app/models/choice';
   styleUrls: ['./ballot.component.css']
 })
 export class BallotComponent implements OnInit {
-
-  usersName = '';
+  
+  submissionState = SubmissionState.Waiting;
+  error = null;
+  name = '';
   anticipatedChoice = '';
   eligibleGames = eligibleGames;
   eligibleRemasters = eligibleRemasters;
@@ -26,18 +30,20 @@ export class BallotComponent implements OnInit {
   }
 
   submit() {
+    this.submissionState = SubmissionState.Submitting
     var ballot = new Ballot();
-    ballot.name = this.usersName;
+    ballot.name = this.name;
     ballot.chosenBestGames = this.chosenGames;
     ballot.chosenBestRemasters = this.chosenRemasters;
     ballot.chosenBiggestDisappointments = this.chosenDisappointments;
     ballot.chosenBiggestSurprises = this.chosenSurprises;
     ballot.chosenMostAnticipated = [this.anticipatedChoice];
     this.submissionService.submit(ballot).subscribe(foo => {
-      console.log(foo);
+      this.submissionState = SubmissionState.Sent;
+    }, (err) => {
+      console.log(err);
+      this.submissionState = SubmissionState.Waiting;
+      this.error = err;
     });
-    console.log(ballot.name);
-    console.log(ballot.chosenMostAnticipated);
   }
-
 }
---
+++ src/app/models/submissionState.ts
@@ -0,0 +1,5 @@
+export enum SubmissionState {
+    Waiting,
+    Submitting,
+    Sent
+} 
